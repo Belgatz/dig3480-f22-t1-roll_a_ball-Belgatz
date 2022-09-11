baf7b918c182a7c15ac1375977d1b674ca145e6a
Belgatz <88949857+Belgatz@users.noreply.github.com>
2022-09-11 15:06:56 +0000

    Sep 11, 2022, 11:06 AM

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -17,6 +17,6 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = player.transform.position + offset;
     }
 }
