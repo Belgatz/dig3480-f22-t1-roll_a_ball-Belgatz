ed89a6150122ec1021cbd1e88d0e19c6c1b30e0b
Belgatz <88949857+Belgatz@users.noreply.github.com>
2022-09-11 17:10:59 +0000

    Sep 11, 2022, 1:10 PM

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -34,6 +34,7 @@ public class PlayerController : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        if(other.gameObject.CompareTag("PickUp"))
         other.gameObject.SetActive(false);
     }
 }
