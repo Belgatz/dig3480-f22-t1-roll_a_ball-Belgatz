4e6d5fa2bd96dbf7d89b65d42c66c85aa1264366
Belgatz <88949857+Belgatz@users.noreply.github.com>
2022-09-17 20:50:02 +0000

    Sep 17, 2022, 4:50 PM

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -64,9 +64,9 @@ public class PlayerController : MonoBehaviour
             SetCountText();
         }
 
-        if (count == 12) 
+        if (count == 12)
         {
-            transform.position = new Vector3(50.0f, 0.0f, 50.0f); 
+            transform.position = new Vector3(50.0f, 0.0f, 0.0f);
         }
     }
 }
