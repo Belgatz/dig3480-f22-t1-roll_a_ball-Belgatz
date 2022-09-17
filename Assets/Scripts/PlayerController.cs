0e2b2db0f2a74c462a1f63ee6a5fc6fdef4c2f1e
Belgatz <88949857+Belgatz@users.noreply.github.com>
2022-09-17 18:23:44 +0000

    Sep 17, 2022, 2:23 PM

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -56,7 +56,7 @@ public class PlayerController : MonoBehaviour
             count = count + 1;
             SetCountText();
         }
-        
+
         if (other.gameObject.CompareTag("Enemy"))
         {
             other.gameObject.SetActive(false);
