fe7cfb8290b0f936ceeac1120e237e1340ad18c0
Belgatz <88949857+Belgatz@users.noreply.github.com>
2022-09-17 23:55:32 +0000

    Sep 17, 2022, 7:55 PM

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -42,6 +42,8 @@ public class PlayerController : MonoBehaviour
         if (count >= 21)
         {
             winTextObject.SetActive(true);
+        
+        
         }
     }
     void FixedUpdate()
